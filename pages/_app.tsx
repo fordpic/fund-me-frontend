31306f47b4db73a3c051130426fbbe38feb02483
Ford Pickert <fordpickert@gmail.com>
2023-07-06 22:02:14 -0500

    connect wallet set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { configureChains, sepolia, createConfig, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+	[sepolia],
+	[publicProvider()]
+);
+
+const config = createConfig({
+	autoConnect: true,
+	publicClient,
+	webSocketPublicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<WagmiConfig config={config}>
+			<Component {...pageProps} />
+		</WagmiConfig>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,4 @@
+import { useState, useEffect } from 'react';
+import Profile from './components/Profile';
+
 export default function Home() {}
